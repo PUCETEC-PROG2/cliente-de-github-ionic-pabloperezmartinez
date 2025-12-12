97dcf7953fd772bce1b09a8b31bb5f146ef828c5
Pablo Pérez Martínez <pabloperezmartinez86@gmail.com>
2025-12-11 21:13:05 -0500

    segregar repositorios por los creados por el usuario logeuado

--- src/services/GithubService.ts
+++ src/services/GithubService.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { RepositoryItem } from "../interfaces/RepositoryItem";
 
 const GITHUB_API_URL = "https://api.github.com";
-const GITHUB_API_TOKEN = "ghp_XXXXXXXXXXXXXXXXXXXXXXX"; // Remplazar con un token válido
+const GITHUB_API_TOKEN = "ghp_XXXXXXXXXXXXXXXXXXXXXXXX"; // Remplazar con un token válido
 
 export const fetchRepositories = async (): Promise<RepositoryItem[]> => {
   try {
@@ -14,6 +14,7 @@ export const fetchRepositories = async (): Promise<RepositoryItem[]> => {
         per_page: 100,
         sort: "created",
         direction: "desc",
+        affiliation: "owner",
       },
     });
 
