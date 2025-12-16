9315ab6e8b6467238eb4fa5a11ce11270fa2a63c
Pablo Pérez Martínez <pabloperezmartinez86@gmail.com>
2025-12-16 11:44:40 -0500

    Separar variables de ambiente y generar pantalla de usuario

--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,8 +1,33 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, useIonViewDidEnter } from '@ionic/react';
 import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/react';
 import './Tab3.css';
+import { useState } from 'react';
+import { getUserInfo } from '../services/GithubService';
 
 const Tab3: React.FC = () => {
+  const [userInfo, setUserInfo] = useState({
+    name: 'No se puede cargar el usuario',
+    username: 'no-username',
+    bio: 'Sin biografía disponible',
+    avatar_url: 'https://ionicframework.com/docs/img/demos/card-media.png'  
+  });
+
+  const loadUserInfo = async () => {
+    const response = await getUserInfo();
+    if (response) {
+      setUserInfo({
+        name: response.name,
+        username: response.login,
+        bio: response.bio || 'Sin biografía disponible',
+        avatar_url: response.avatar_url || 'https://ionicframework.com/docs/img/demos/card-media.png'
+      });
+    }
+  }
+
+  useIonViewDidEnter(() => {
+    loadUserInfo();
+  }, []);
+
   return (
     <IonPage>
       <IonHeader>
@@ -18,12 +43,12 @@ const Tab3: React.FC = () => {
         </IonHeader>
         <div className="card-container">
           <IonCard className="card">
-            <img alt="Silhouette of mountains" src="https://ionicframework.com/docs/img/demos/card-media.png" />
+            <img alt="Silhouette of mountains" src={userInfo.avatar_url} />
             <IonCardHeader>
-              <IonCardTitle>Pablo Pérez Martínez</IonCardTitle>
-              <IonCardSubtitle>pabloperezmartinez</IonCardSubtitle>
+              <IonCardTitle>{userInfo.name}</IonCardTitle>
+              <IonCardSubtitle>{userInfo.username}</IonCardSubtitle>
             </IonCardHeader>
-            <IonCardContent>Este es el perfil de Pablo Pérez Martínez</IonCardContent>
+            <IonCardContent>{userInfo.bio}</IonCardContent>
           </IonCard>
         </div>
       </IonContent>
--- src/services/GithubService.ts
+++ src/services/GithubService.ts
@@ -1,8 +1,9 @@
 import axios from "axios";
 import { RepositoryItem } from "../interfaces/RepositoryItem";
+import { UserInfo } from "../interfaces/UserInfo";
 
-const GITHUB_API_URL = "https://api.github.com";
-const GITHUB_API_TOKEN = "ghp_XXXXXXXXXXXXXXXXXXXXXXX"; // Remplazar con un token válido
+const GITHUB_API_URL = import.meta.env.VITE_GITHUB_API_URL || "https://api.github.com";
+const GITHUB_API_TOKEN = import.meta.env.VITE_GITHUB_API_TOKEN || "";
 
 /**
  * Obtener repositorios del usuario autenticado
@@ -51,5 +52,19 @@ export const createRepository = async (repo : RepositoryItem): Promise<void> => 
     console.log("Repositorio ingresado ", response.data);
   } catch (error) {
     console.error("Error creating repository:", error);
+  }
+};
+
+export const getUserInfo = async (): Promise<UserInfo | null> => {
+  try {
+    const response = await axios.get(`${GITHUB_API_URL}/user`, {
+      headers: {
+        Authorization: `Bearer ${GITHUB_API_TOKEN}`,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching user info:", error);
+    return null;
   }
 };
